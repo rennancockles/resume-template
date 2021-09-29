f63c3e4f1d368dba7653015e6bcfe22da8016162
Rennan Cockles <rennancockles@gmail.com>
2021-09-29 11:59:56 -0300

    turn resume into a component

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,20 @@
 <template>
   <v-app class="printable">
     <v-main>
-      <v-container fluid class="pa-0">
-        <v-row no-gutters class="my-nav white--text elevation-5">
-          <v-col cols="12">
-            {{jsonData.name}}
-            <div class="subheading">
-              {{jsonData.title}}
-            </div>
-          </v-col>
-        </v-row>
-        <v-row no-gutters>
-          <v-col cols="3" class="grey darken-3">
-            <Contact :data="jsonData.contact"/>
-            <Languages :data="jsonData.languages"/>
-            <Certifications :data="jsonData.certifications"/>
-          </v-col>
-          <v-col cols="9">
-            <Summary :summary="jsonData.summary"/>
-            <Education :data="jsonData.education"/>
-            <WorkExperience :data="jsonData.workExperience"/>
-          </v-col>
-        </v-row>
-      </v-container>
+      <Resume :data="jsonData" language="pt"/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import Contact from '@/components/sections/Contact'
-import Languages from '@/components/sections/Languages'
-import Certifications from '@/components/sections/Certifications'
-import Summary from '@/components/sections/Summary'
-import Education from '@/components/sections/Education'
-import WorkExperience from '@/components/sections/WorkExperience'
+import Resume from '@/components/Resume'
 import json from './data/pt.json'
 
 export default {
   name: 'App',
 
   components: {
-    Contact,
-    Languages,
-    Certifications,
-    Summary,
-    Education,
-    WorkExperience
+    Resume
   },
 
   data: () => ({
---
+++ src/components/Resume.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-container fluid class="pa-0">
+    <v-row no-gutters class="my-nav white--text elevation-5">
+      <v-col cols="12">
+        {{data.name}}
+        <div class="subheading">
+          {{data.title}}
+        </div>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col cols="3" class="grey darken-3">
+        <Contact :data="data.contact"/>
+        <Languages :data="data.languages"/>
+        <Certifications :data="data.certifications"/>
+      </v-col>
+      <v-col cols="9">
+        <Summary :summary="data.summary"/>
+        <Education :data="data.education"/>
+        <WorkExperience :data="data.workExperience"/>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import Contact from '@/components/sections/Contact'
+import Languages from '@/components/sections/Languages'
+import Certifications from '@/components/sections/Certifications'
+import Summary from '@/components/sections/Summary'
+import Education from '@/components/sections/Education'
+import WorkExperience from '@/components/sections/WorkExperience'
+
+export default {
+  name: 'App',
+
+  props: {
+    data: {
+      type: Object,
+      default: function () {
+        return {}
+      }
+    },
+    language: {
+      type: String,
+      default: 'pt'
+    }
+  },
+
+  components: {
+    Contact,
+    Languages,
+    Certifications,
+    Summary,
+    Education,
+    WorkExperience
+  }
+}
+</script>
