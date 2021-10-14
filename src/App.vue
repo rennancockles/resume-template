ef46c1c642a8506f676c8f3d8d6f90c985db0a7d
Rennan Cockles <rennancockles@gmail.com>
2021-10-14 20:05:40 -0300

    resume css responsive

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,12 @@
 <template>
   <v-app class="printable">
-    <v-main>
-      <v-container fluid class="pa-0">
-        <v-row no-gutters class="my-nav white--text elevation-5">
-          <v-col
-          cols="2"
-          align-self="center"
-          class="pl-2 d-flex d-print-none justify-start"
-          >
-            <v-btn
-            id="print-btn"
-            outlined
-            dark
-            @click="printWindow()"
-            >
-              <v-icon left>
-                mdi-printer-outline
-              </v-icon>
-              {{$t('print')}}
-            </v-btn>
-          </v-col>
-          <v-col cols="8" class="mx-auto">
-            {{jsonData.name}}
-            <div class="subheading">
-              {{jsonData.title}}
-            </div>
-          </v-col>
-          <v-col
-          cols="2"
-          align-self="center"
-          class="pr-2 d-flex d-print-none justify-end"
-          >
-            <v-select
-            v-model="locale"
-            :items="locales"
-            class="locale-select"
-            prepend-inner-icon="mdi-translate"
-            menu-props="auto"
-            hide-details
-            dark
-            outlined
-            ></v-select>
-          </v-col>
-        </v-row>
-
-        <Resume :data="jsonData"/>
-      </v-container>
-    </v-main>
+    <Resume
+      :data="jsonData"
+      :locales="locales"
+      :activeLocale="locale"
+      @changeLocale="changeLocale"
+      @print="print"
+    />
   </v-app>
 </template>
 
@@ -100,7 +60,10 @@ export default {
         this.locale = this.localeData[0]
       }
     },
-    printWindow () {
+    changeLocale (value) {
+      this.locale = value
+    },
+    print () {
       window.print()
     }
   },
@@ -112,90 +75,3 @@ export default {
   }
 }
 </script>
-
-<style>
-.printable {
-  -webkit-print-color-adjust: exact;
-  color-adjust: exact;
-}
-
-.wrap-text {
-  word-wrap: break-word;
-}
-
-.my-nav {
-  align-self: center;
-  text-align: center;
-  font-size: 3rem;
-  padding: 0;
-  background-color: #6A76AB;
-  position: sticky;
-  top: 0;
-  width: 100%;
-  z-index: 5;
-}
-
-.my-nav .subheading {
-  text-align: center;
-  font-size: 1.15rem;
-  padding-right: 5px;
-}
-
-.my-nav .locale-select {
-  min-width: 110px;
-  max-width: 120px;
-}
-
-.v-subheader {
-  font-size: 1.5rem;
-  color: #fff;
-}
-.theme--dark.v-subheader {
-  color: #fff;
-}
-
-.v-list-item__icon {
-  margin-right: 10px !important;
-}
-
-.v-list-item--three-line .wrap-text {
-  -webkit-line-clamp: 3;
-}
-
-#print-btn {
-  border-color: rgba(255, 255, 255, 0.27);
-}
-
-#print-btn:hover {
-  border-color: rgba(255, 255, 255, 1);
-}
-
-#print-btn:hover::before {
-  opacity: 0;
-}
-
-@media print {
-  @page {
-    margin: 0;
-    padding: 0;
-    size: A4;
-  }
-  /* html {
-    height: 125%;
-    width: 125%;
-
-    transform: translate(-10%, -10%) scale(0.8, 0.8);
-    -moz-transform: translate(-10%, -10%) scale(0.8, 0.8);
-    -ms-transform: translate(-10%, -10%) scale(0.8, 0.8);
-    -webkit-transform: translate(-10%, -10%) scale(0.8, 0.8);
-    -o-transform: translate(-10%, -10%) scale(0.8, 0.8);
-  } */
-
-  .my-nav.elevation-5{
-    box-shadow: none !important;
-  }
-  .v-application .my-card{
-    box-shadow: none !important;
-  }
-}
-</style>
--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,16 +1,84 @@
 <template>
-  <v-row no-gutters>
-    <v-col cols="3" class="grey darken-3">
+  <div id="resume">
+    <v-app-bar
+      app
+      dark
+      clipped-left
+      prominent
+      color="#6A76AB"
+    >
+      <v-app-bar-nav-icon
+      @click.stop="showDrawer = !showDrawer"
+      class="hidden-md-and-up d-print-none"></v-app-bar-nav-icon>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-title>
+        Rennan Cockles
+        <div class="subheading">
+          Software Engineer / Data Engineer
+        </div>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-menu
+        open-on-hover
+        transition="slide-y-transition"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            id="btn-menu"
+            icon
+            v-bind="attrs"
+            v-on="on"
+            class="d-print-none"
+          >
+            <v-icon>mdi-translate</v-icon>
+            <span v-text="activeLocale"></span>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item-group
+            v-model="selectedItem"
+            color="info"
+          >
+            <v-list-item
+              v-for="locale in locales"
+              :key="locale"
+              @click="changeLocale(locale)"
+            >
+              <v-list-item-title v-text="locale"></v-list-item-title>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-menu>
+
+      <v-btn icon @click="print" class="d-print-none">
+        <v-icon>mdi-printer-outline</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      fixed
+      clipped
+      v-model="showDrawer"
+      :temporary="$vuetify.breakpoint.smAndDown"
+      :permanent="$vuetify.breakpoint.mdAndUp"
+      app
+      >
       <Contact :data="data.contact"/>
       <Languages :data="data.languages"/>
       <Certifications :data="data.certifications"/>
-    </v-col>
-    <v-col cols="9">
+    </v-navigation-drawer>
+
+    <v-main>
       <Summary :summary="data.summary"/>
       <Education :data="data.education"/>
       <WorkExperience :data="data.workExperience"/>
-    </v-col>
-  </v-row>
+    </v-main>
+  </div>
 </template>
 
 <script>
@@ -22,7 +90,7 @@ import Education from '@/components/sections/Education'
 import WorkExperience from '@/components/sections/WorkExperience'
 
 export default {
-  name: 'App',
+  name: 'Resume',
 
   props: {
     data: {
@@ -30,6 +98,18 @@ export default {
       default: function () {
         return {}
       }
+    },
+
+    locales: {
+      type: Array,
+      default: function () {
+        return []
+      }
+    },
+
+    activeLocale: {
+      type: String,
+      default: ''
     }
   },
 
@@ -40,6 +120,60 @@ export default {
     Summary,
     Education,
     WorkExperience
+  },
+
+  data: () => ({
+    selectedItem: null,
+    showDrawer: false
+  }),
+
+  created () {
+    this.selectedItem = this.locales.indexOf(this.activeLocale)
+  },
+
+  methods: {
+    changeLocale (locale) {
+      this.$emit('changeLocale', locale)
+    },
+    print () {
+      this.$emit('print')
+    }
   }
 }
 </script>
+
+<style>
+#resume .v-toolbar--prominent .v-toolbar__title {
+  text-align: center;
+  font-size: min(6vw, 2.5rem);
+}
+#resume .v-toolbar--prominent .v-toolbar__title .subheading {
+  font-size: min(3.5vw, 1.5rem);
+}
+#btn-menu .v-btn__content {
+  flex-direction: column;
+  align-items: end;
+}
+#btn-menu .v-btn__content > span {
+  font-size: 0.5rem;
+}
+
+.v-list-item--three-line .wrap-text {
+  -webkit-line-clamp: 3;
+}
+
+.wrap-text {
+  word-wrap: break-word;
+}
+
+.v-subheader {
+  font-size: 1.5rem;
+}
+.theme--dark.v-subheader {
+  color: #fff;
+}
+
+.v-list-item__icon {
+  margin-right: 10px !important;
+}
+</style>
