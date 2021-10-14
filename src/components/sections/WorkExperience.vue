a1121b06182fcd2cdaac598399009af83caeacd1
Rennan Cockles <rennancockles@gmail.com>
2021-10-14 17:35:03 -0300

    workExperience css responsive

--- src/components/sections/WorkExperience.vue
+++ src/components/sections/WorkExperience.vue
@@ -18,7 +18,57 @@
           v-for="(item, i) in filteredData"
           :key="i"
           >
-            <v-row no-gutters>
+            <v-list-item-content
+            class="stacked-view hidden-sm-and-up"
+            >
+              <v-list-item-title
+              class="wrap-text"
+              v-text="item.position"
+              ></v-list-item-title>
+
+              <v-list-item-subtitle
+              v-text="formatDate(item.startDate, item.endDate)"
+              ></v-list-item-subtitle>
+
+              <v-list-item-subtitle
+              class="employer"
+              v-text="item.employer"
+              ></v-list-item-subtitle>
+
+              <v-list-item-subtitle
+              class="wrap-text"
+              >
+                {{ item.location }}
+                <span v-if="item.remote">
+                  • {{ $t('remote') }}
+                </span>
+              </v-list-item-subtitle>
+
+              <v-list-item-action-text
+              class="wrap-text description"
+              >
+                <p
+                v-for="(line, i) in splitLines(item.description)"
+                :key="i"
+                v-text="line"
+                ></p>
+              </v-list-item-action-text>
+
+              <v-list-item-action-text
+              class="wrap-text"
+              v-if="item.tools"
+              >
+                <span class="font-weight-bold">
+                  {{$t('workTools')}}:
+                </span>
+                {{item.tools}}
+              </v-list-item-action-text>
+            </v-list-item-content>
+
+            <v-row
+            no-gutters
+            class="normal-view hidden-xs-only"
+            >
               <v-col cols="3">
                 <v-list-item-icon>
                   <v-list-item-title
@@ -120,22 +170,42 @@ p {
   margin: 0;
 }
 
-.v-list-item__icon {
+.v-list-item__action-text {
+  margin-top: 10px;
+}
+
+.v-list-item__content.stacked-view .v-list-item__title,
+.v-list-item__content.stacked-view .v-list-item__subtitle.employer {
+  font-size: 3vw;
+}
+.v-list-item__content.stacked-view .v-list-item__subtitle {
+  font-size: 2.625vw;
+}
+.v-list-item__content.stacked-view .v-list-item__action-text {
+  font-size: 2.5vw;
+}
+
+div.normal-view .v-list-item__icon {
   flex-wrap: wrap;
   width: 150px;
 }
-
-.v-list-item__content {
+div.normal-view .v-list-item__content {
   margin: 16px 0 16px 32px;
   padding: 0;
   align-self: flex-start;
 }
 
-.v-list-item__action-text {
-  margin-top: 10px;
+div.normal-view .v-list-item__title,
+div.normal-view .v-list-item__subtitle.employer {
+  font-size: min(2vw, 1rem);
 }
-
-.v-list-item__icon {
+div.normal-view .v-list-item__subtitle {
+  font-size: min(1.8vw, 0.875rem);
+}
+div.normal-view .v-list-item__action-text {
+  font-size: min(1.8vw, 0.75rem);
+}
+div.normal-view .v-list-item__icon {
   width: 100%;
 }
 </style>
